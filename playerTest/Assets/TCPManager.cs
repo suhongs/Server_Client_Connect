f95263739d9b1cc7404e7b4008d1a742df423762
suhongs <rlatnghd1236@gmail.com>
2020-10-10 19:05:21 +0900

    Unity Client Connect

---
+++ playerTest/Assets/TCPManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityNetwork;
+using UnityEngine.UI;
+using System.Linq.Expressions;
+
+public class TCPManager : MonoBehaviour
+{
+    private NetworkStream m_networkStream;
+    private TcpClient m_client;
+
+    private byte[] sendBuffer = new byte[1024 * 4];
+    private byte[] readBuffer = new byte[1024 * 4];
+    private bool m_bConnect = false;
+    public Login m_loginClass;
+
+    public playUserInfo player;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void Send()
+    {
+        this.m_networkStream.Write(this.sendBuffer, 0, this.sendBuffer.Length);
+        this.m_networkStream.Flush();
+
+        for(int i=0; i<1024; i++)
+        {
+            this.sendBuffer[i] = 0;
+        }
+    }
+
+    public void connect()
+    {
+        this.m_client = new TcpClient();
+        try
+        {
+            this.m_client.Connect("127.0.0.1", 8000);
+        }
+        catch
+        {
+            Debug.Log("접속 에러");
+        }
+
+        this.m_bConnect = true;
+        this.m_networkStream = this.m_client.GetStream();
+    }
+
+    public void nameSend()
+    {
+        if(!this.m_bConnect)
+        {
+            return;
+        }
+
+        Login login = new Login();
+        login.Type = (int)PacketType.접속;
+        login.userName = player.userName;
+
+        Packet.Serialize(login).CopyTo(this.sendBuffer, 0);
+        this.Send();
+    }
+
+}
---
+++ playerTest/Assets/btnController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btnController : MonoBehaviour
+{
+    public InputField userName;
+    public playUserInfo player;
+    public Text statusText;
+
+    public TCPManager man;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void btnClick()
+    {
+        if (userName.text == "")
+        {
+            statusText.text = "닉네임을 입력하세요";
+            return;
+        }
+
+        statusText.text = "서버에 접속중입니다...";
+        
+        player.userName = System.Convert.ToString(userName.text);
+
+        man.connect();
+        man.nameSend();
+
+
+    }
+}
